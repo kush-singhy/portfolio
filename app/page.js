cd174884f5d86e1d0bf240a130add607fad05938
Kush <153984588+kush-singhy@users.noreply.github.com>
2024-12-12 15:42:53 +1100

    Tech stack icons

--- app/page.js
+++ app/page.js
@@ -1,18 +1,13 @@
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
+import TechStack from '@/components/TechStack';
 
 export default function Home() {
   return (
     <div className="max-w-screen-lg flex flex-col justify-center">
       <Navbar />
       <Hero />
-
-      <div id="tech-stack">
-        <h2>Tech Stack</h2>
-        <div id="tech-stack-icon"></div>
-        <div id="tech-stack-icon"></div>
-        <div id="tech-stack-icon"></div>
-      </div>
+      <TechStack />
     </div>
   );
 }
---
+++ components/TechStack.js
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+
+import reactIcon from '@/assets/react.svg';
+import tailwindIcon from '@/assets/tailwind.svg';
+import javascriptIcon from '@/assets/javascript.svg';
+import nodeIcon from '@/assets/nodejs.svg';
+import postgresqlIcon from '@/assets/postgres.svg';
+import figmaIcon from '@/assets/figma.svg';
+import ec2Icon from '@/assets/ec2.svg';
+import route53Icon from '@/assets/route53.svg';
+
+export default function TechStack() {
+  return (
+    <div id="tech-stack" className="flex flex-col items-center justify-center">
+      <h2 className="text-xl font-medium border border-gray-900 rounded-full px-4 py-1 mb-7">
+        Tech Stack
+      </h2>
+      <div id="tech-stack-icons" className="grid grid-cols-8 gap-4">
+        <TechStackIcon src={javascriptIcon} text="Javascript" />
+        <TechStackIcon src={reactIcon} text="React" />
+        <TechStackIcon src={tailwindIcon} text="TailwindCSS" />
+        <TechStackIcon src={nodeIcon} text="NodeJS" />
+        <TechStackIcon src={postgresqlIcon} text="PostgreSQL" />
+        <TechStackIcon src={figmaIcon} text="Figma" />
+        <TechStackIcon src={ec2Icon} text="AWS EC2" />
+        <TechStackIcon src={route53Icon} text="AWS Route 53" />
+      </div>
+    </div>
+  );
+}
+
+function TechStackIcon({ src, text }) {
+  return (
+    <div className="tech-stack-icon group">
+      <Image src={src} alt={text} className="w-9 h-9" />
+
+      <span className="tech-stack-text group-hover:scale-100">{text}</span>
+    </div>
+  );
+}
